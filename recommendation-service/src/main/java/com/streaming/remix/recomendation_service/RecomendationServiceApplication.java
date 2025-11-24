2e7a0f1c8b0d3a6a143b10dfec0625688fd572a2
sui7ca <guilherme.silva9@br.bosch.com>
2025-11-24 16:38:36 -0300

    Initial commit

---
+++ recommendation-service/src/main/java/com/streaming/remix/recomendation_service/RecomendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.remix.recomendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationServiceApplication.class, args);
+	}
+
+}
---
+++ recommendation-service/src/test/java/com/streaming/remix/recomendation_service/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.remix.recomendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
